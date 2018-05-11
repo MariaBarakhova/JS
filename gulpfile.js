6ba31d09f82e35fc37b15cbf3b6835643d336aee
DzeNb <KovalenkoDMY@gmail.com>
2018-05-11 21:25:59 +0300

    changes2

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,30 @@ const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const plumber = require('gulp-plumber');
 const rename = require('gulp-rename');
+const livereload = require('gulp-livereload');
 const gulpminifycss = require('gulp-minify-css');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+
+const config = {
+server:{
+    baseDir: './build'
+},
+host: 'localhost',
+port: '9000',
+
+};
 
 
+gulp.task('webserver', function(){
+    browserSync(config);
+});
+
+gulp.task ('html', function(){
+    return gulp.src('./src/index.html')
+    .pipe(gulp.dest('./build'))
+    .pipe(reload({stream: true}))
+});
 
 gulp.task('css', function(){
     return gulp.src('css/main.css')
@@ -26,6 +47,10 @@ gulp.task('sass', function() {
 });
 
 gulp.task('watch', function(){
-gulp.watch('sass/**/*.scss', ['sass']);
-gulp.watch('css/main.css', ['css']);
+    gulp.watch('./src/**/*.html', ['html']);
+    gulp.watch('sass/**/*.scss', ['sass']);
+    gulp.watch('css/main.css', ['css']);
 });
+
+gulp.task ('build', ['html'])
+gulp.task('default', ['build', 'webserver', 'watch']);
