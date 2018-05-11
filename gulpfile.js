b4b2e9f2b17c2cc75a4fedaeffdda891a0280147
DzeNb <KovalenkoDMY@gmail.com>
2018-05-11 20:08:14 +0300

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,31 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const watch = require('gulp-watch');
+const autoprefixer = require('gulp-autoprefixer');
+const plumber = require('gulp-plumber');
+const rename = require('gulp-rename');
+const gulpminifycss = require('gulp-minify-css');
 
-gulp.task('sass', function() {
-    return gulp.src('sass/style.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('sass/'))
+
+
+gulp.task('css', function(){
+    return gulp.src('css/main.css')
+    .pipe(gulpminifycss())
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest('css/'))
 });
 
-gulp.task('watch', function() {
-    gulp.watch('sass/**/*.scss', ['sass']);
+gulp.task('sass', function() {
+    return gulp.src('sass/**/*.scss')
+    .pipe(plumber())
+   .pipe (autoprefixer({
+       browsers:['last 2 version'],
+       casced: false,
+   }))
+    .pipe(sass())
+    .pipe(gulp.dest('css/'))
+});
+
+gulp.task('watch', function(){
+gulp.watch('sass/**/*.scss', ['sass']);
+gulp.watch('css/main.css', ['css']);
 });
